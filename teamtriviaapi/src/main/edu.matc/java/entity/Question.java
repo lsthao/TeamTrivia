ee4a2496083b4beb82a00fc96ed32c4f0ea13d41
Ren Thao <lthao4@madisoncollege.edu>
2017-10-29 01:47:00 -0500

    updated gitign/question&DAO/ses
    Added SesFact.java, DB.java and Question entity & DAO. added DB.prop and hibernate.cfg.xml to GITIGNORE to not mix up db connectors.

--- teamtriviaapi/src/main/edu.matc/java/entity/Question.java
+++ teamtriviaapi/src/main/edu.matc/java/entity/Question.java
@@ -1,4 +1,21 @@
 package entity;
 
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "question")
+
 public class Question {
+
+    @Id
+    @GeneratedValue (generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "QuestionID")
+    private int QuestionID;
+
+    @Column
+    private String Question;
+
+
 }
---
+++ teamtriviaapi/src/main/edu.matc/java/persistence/QuestionDAO.java
@@ -0,0 +1,124 @@
+package persistence;
+
+import entity.Question;
+import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.Criteria;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionDAO {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+
+    public List<Question> getAllQuestions() {
+        List<Question> questions = new ArrayList<Question>();
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            questions = session.createCriteria(Question.class).list();
+        } catch (HibernateException he) {
+            log.error("Error getting all Questions", he);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return questions;
+    }
+
+    public Question getQuestionById(int questionId) {
+        Question question = null;
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            question = (Question) session.get(Question.class, questionId);
+        } catch (HibernateException he) {
+            log.error("Error getting Question with id: " + questionId, he);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return question;
+    }
+
+    public void updateQuestion(Question question) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(question);
+            transaction.commit();
+        } catch (HibernateException he){
+            if (transaction != null) {
+                try {
+                    transaction.rollback();
+                } catch (HibernateException he2) {
+                    log.error("Error rolling back save of question: " + question, he2);
+                }
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public int insertQuestion(Question question) {
+        int id = 0;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            id = (int) session.save(question);
+            transaction.commit();
+        } catch (HibernateException he){
+            if (transaction != null) {
+                try {
+                    transaction.rollback();
+                } catch (HibernateException he2) {
+                    log.error("Error rolling back save of question: " + question, he2);
+                }
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return id;
+    }
+
+    public void deleteByQuestionId(int id) {
+        Transaction transaction = null;
+        Session session = null;
+        Question question = null;
+        try {
+
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            question = (Question) session.get(Question.class, id);
+            transaction = session.beginTransaction();
+            session.delete(question);
+            transaction.commit();
+        } catch (HibernateException he){
+            if (transaction != null) {
+                try {
+                    transaction.rollback();
+                } catch (HibernateException he2) {
+                    log.error("Error rolling back save of question: " + question, he2);
+                }
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+
+}
