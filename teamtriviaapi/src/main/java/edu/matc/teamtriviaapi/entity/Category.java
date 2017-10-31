74247c71f66da9583f91d01f0c330f4ca28850c1
kvang5 <kvang0323@gmail.com>
2017-10-31 14:14:09 -0500

    added pojo for entities and some dao classes.

--- teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/entity/Category.java
+++ teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/entity/Category.java
@@ -1,4 +1,58 @@
 package edu.matc.teamtriviaapi.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private int CategoryID;
+
+    @Column(name = "CategoryName")
+    private String CategoryName;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Category_CategoryID")
+    private Question question;
+
+
+    public Category() {
+
+    }
+
+    public int getCategoryID() {
+        return CategoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        CategoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return CategoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "CategoryID=" + CategoryID +
+                ", CategoryName='" + CategoryName + '\'' +
+                '}';
+    }
 }
--- teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/entity/Question.java
+++ teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/entity/Question.java
@@ -17,5 +17,4 @@ public class Question {
     @Column
     private String Question;
 
-
 }
