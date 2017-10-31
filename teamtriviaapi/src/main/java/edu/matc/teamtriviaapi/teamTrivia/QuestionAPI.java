a1bc4d956964000ef7257381ecd8fda33568df4f
somernik <somernik@madisoncollege.edu>
2017-10-31 14:19:23 -0500

    Adding basic API functionality

---
+++ teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/teamTrivia/QuestionAPI.java
@@ -0,0 +1,77 @@
+package edu.matc.teamtriviaapi.teamTrivia;
+/**
+ * Created by sarah on 10/31/2017.
+ */
+import edu.matc.teamtriviaapi.entity.Question;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Path("/questions")
+public class QuestionAPI {
+    // The Java method will process HTTP GET requests
+    @GET
+    // The Java method will produce content identified by the MIME Media type "text/plain"
+    @Produces("text/html")
+    public Response getMessage() {
+        // Return a simple message
+        String output = "Welcome to the questions API";
+        return Response.status(200).entity(output).build();
+    }
+
+    @GET
+    @Produces({"text/html", "text/plain"})
+    @Path("/{id}")
+    public Response getQuestion(@PathParam("id") String id) {
+        if (!isNumeric(id)) {
+            return Response.status(400).entity("Ids should be numeric").build();
+        }
+
+        Question question = new Question(); // TODO get question from database
+        String output ="Question with " + id + " here";
+
+        if (question != null) {
+            // TODO question to string
+            return Response.status(200).entity(output).build();
+
+        } else {
+            output = "Question does not exist";
+            return Response.status(404).entity(output).build();
+        }
+    }
+
+    @GET
+    @Produces({"text/html", "text/plain"})
+    @Path("/query")
+    public Response getManyQuestions(@QueryParam("type") String type, @QueryParam("category") String category,
+                                     @QueryParam("amount") String amount, @QueryParam("difficulty") String difficulty) {
+
+        // TODO check inputs
+
+        List<Question> questions = new ArrayList<Question>();
+        // TODO get questions from database
+        String output ="Questions here : amount-" + amount + "; difficulty- " +difficulty+ "; type- " +type +  "; category- " + category;
+
+        if (questions != null) {
+            // TODO question to string
+            return Response.status(200).entity(output).build();
+
+        } else {
+            output = "Question does not exist";
+            return Response.status(404).entity(output).build();
+        }
+    }
+
+    public static boolean isNumeric(String str)
+    {
+        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+
+}
