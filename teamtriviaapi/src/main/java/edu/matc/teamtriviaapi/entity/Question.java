ba65c1fe8d4f9e9d9de27a19f316d6351a2566e7
Ren Thao <lthao4@madisoncollege.edu>
2017-10-31 14:01:59 -0500

    updated Question Entity
    completed mapping

--- teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/entity/Question.java
+++ teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/entity/Question.java
@@ -12,10 +12,78 @@ public class Question {
     @GeneratedValue (generator = "increment")
     @GenericGenerator(name = "increment", strategy = "increment")
     @Column(name = "QuestionID")
-    private int QuestionID;
+    private int questionId;
 
-    @Column
-    private String Question;
+    @Column (name = "Question")
+    private String question;
+
+    @Column(name = "Category_CategoryID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CategoryID", nullable = false)
+    private Category category;
+
+    @Column(name = "Type_TypeID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TypeID", nullable = false)
+    private Type type;
+
+
+    @Column(name = "Difficulty_DifficultyID")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DifficultyID", nullable = false)
+    private Difficulty difficulty;
+
+
+
+    public Question() {
+        this.questionId = questionId;
+        this.question = question;
+        this.category = category;
+        this.type = type;
+        this.difficulty = difficulty;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+           this.questionId = questionId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion() {
+        this.questionId = questionId;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory() {
+        this.category = category;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType() {
+        this.type = type;
+    }
+
+    public Difficulty getDifficulty()   {
+        return difficulty;
+    }
+
+    public void setDifficulty() {
+        this.difficulty = difficulty;
+    }
+
+
 
 
 }
