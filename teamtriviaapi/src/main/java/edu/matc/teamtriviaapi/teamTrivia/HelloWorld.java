470b16db425a935377b1f11c2b22394b582fd089
somernik <somernik@madisoncollege.edu>
2017-11-16 12:12:34 -0600

    Added comment to findbyproperty for clarity

--- teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/teamTrivia/HelloWorld.java
+++ teamtriviaapi/src/main/java/edu/matc/teamtriviaapi/teamTrivia/HelloWorld.java
@@ -11,7 +11,6 @@ import javax.ws.rs.core.Response;
 public class HelloWorld {
     // The Java method will process HTTP GET requests
     @GET
-    // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
     public Response getMessage() {
         // Return a simple message
